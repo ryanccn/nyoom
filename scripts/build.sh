6d71ea60f9c571e1308604139dd0a1942fb9b1c1
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-18 09:00:31 +0800

    ci: add optimized RUSTFLAGS to standard build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,22 +2,23 @@
 set -eo pipefail
 
 apt_updated=""
-apt_update() {
+apt_install() {
     if [ ! "$apt_updated" ]; then
         sudo apt-get update
         apt_updated="y"
     fi
+
+    sudo apt-get install "$@"
 }
 
-if [[ "$TARGET" = "aarch64-unknown-linux-"* ]]; then
-    apt_update
-    sudo apt-get install gcc-aarch64-linux-gnu
+export RUSTFLAGS="-C strip=symbols -C codegen-units=1 -C opt-level=z"
 
-    export RUSTFLAGS="--codegen linker=aarch64-linux-gnu-gcc"
+if [[ "$TARGET" = "aarch64-unknown-linux-"* ]]; then
+    apt_install gcc-aarch64-linux-gnu
+    export RUSTFLAGS="$RUSTFLAGS --codegen linker=aarch64-linux-gnu-gcc"
 fi
 if [[ "$TARGET" = *"-linux-musl" ]]; then
-    apt_update
-    sudo apt-get install musl-dev
+    apt_install musl-dev
 fi
 
 rustup target add "$TARGET"
