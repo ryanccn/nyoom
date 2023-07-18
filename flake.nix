cf2c742b75dc99c28056de056651e03de6a7f7af
seth <getchoo@tuta.io>
2023-07-17 20:24:46 -0400

    feat(nix): add static musl builds

--- flake.nix
+++ flake.nix
@@ -2,27 +2,43 @@
   description = "A small CLI Firefox userchrome manager";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
   };
 
   outputs =
     { self
     , nixpkgs
+    , crane
     , rust-overlay
     , ...
     }:
     let
       version = builtins.substring 0 8 self.lastModifiedDate;
 
-      systems = [
+      linux = [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+      systems =
+        [
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+        ++ linux;
+
+      inherit (nixpkgs) lib;
+
+      forAllSystems = lib.genAttrs systems;
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
           inherit system;
@@ -35,16 +51,30 @@
             inherit system;
             pkgs = nixpkgsFor.${system};
           });
+
+      toolchainFor = forEachSystem (
+        { pkgs
+        , system
+        , ...
+        }:
+        pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+          targets = lib.optionals (builtins.elem system linux) [
+            "x86_64-unknown-linux-musl"
+            "aarch64-unknown-linux-musl"
+          ];
+        }
+      );
     in
     {
-      devShells = forEachSystem ({ pkgs, ... }:
-        let
-          inherit (pkgs) mkShell;
-        in
-        {
-          default = mkShell {
+      devShells = forEachSystem
+        ({ pkgs
+         , system
+         , ...
+         }: {
+          default = pkgs.mkShell {
             packages = with pkgs; [
-              rust-bin.stable.latest.default
+              toolchainFor.${system}
               rust-analyzer
             ];
 
@@ -52,29 +82,63 @@
           };
         });
 
-      formatter = forEachSystem ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+      formatter = forEachSystem (p: p.pkgs.nixpkgs-fmt);
 
-      packages = forEachSystem ({ pkgs, ... }: {
-        inherit (pkgs) nyoom;
-        default = pkgs.nyoom;
-      });
+      packages = forEachSystem
+        ({ pkgs
+         , system
+         , ...
+         }:
+          lib.recursiveUpdate
+            {
+              inherit (pkgs) nyoom;
+              default = pkgs.nyoom;
+            }
+            (
+              lib.optionalAttrs (builtins.elem system linux) {
+                nyoom-static =
+                  let
+                    arch =
+                      if system == "x86_64-linux"
+                      then "x86_64"
+                      else if system == "aarch64-linux"
+                      then "aarch64"
+                      else "";
 
-      overlays.default = _: prev: {
-        nyoom = prev.pkgs.rustPlatform.buildRustPackage
-          {
-            pname = "nyoom";
-            inherit version;
+                    flags.CARGO_BUILD_TARGET = "${arch}-unknown-linux-musl";
+                    flags."CARGO_TARGET_${lib.toUpper arch}_UNKNOWN_LINUX_MUSL_LINKER" =
+                      let
+                        inherit (pkgs.pkgsStatic.stdenv) cc;
+                      in
+                      "${cc}/bin/${cc.targetPrefix}cc";
+                  in
+                  pkgs.nyoom.overrideAttrs (_:
+                    {
+                      # optimize for size
+                      CARGO_BUILD_RUSTFLAGS = "-C strip=symbols -C target-feature=+crt-static -C opt-level=z -C codegen-units=1";
+                    }
+                    // flags);
+              }
+            ));
 
-            src = self;
-
-            cargoLock.lockFile = "${self}/Cargo.lock";
-
-            buildInputs = with prev.pkgs; [ ];
-
-            nativeBuildInputs = with prev.pkgs; [
-              pkg-config
-            ];
-          };
-      };
+      overlays.default = _: prev:
+        let
+          crane' = crane.mkLib prev;
+        in
+        {
+          nyoom =
+            let
+              isLinux = builtins.elem prev.system linux;
+              buildRustPackage =
+                if isLinux
+                then (crane'.overrideToolchain toolchainFor.${prev.system}).buildPackage
+                else crane'.buildPackage;
+            in
+            buildRustPackage {
+              pname = "nyoom";
+              inherit version;
+              src = self;
+            };
+        };
     };
 }
