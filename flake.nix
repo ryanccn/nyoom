5cc8a245182e4d5f7bba171197ea42358dce715a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-20 11:12:43 +0800

    nix: clean source with crane

--- flake.nix
+++ flake.nix
@@ -10,103 +10,125 @@
     ];
   };
 
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , rust-overlay
-    }:
-    let
-      version = builtins.substring 0 8 self.lastModifiedDate or "dirty";
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    crane,
+  }: let
+    version = builtins.substring 0 8 self.lastModifiedDate or "dirty";
 
-      inherit (nixpkgs) lib;
+    inherit (nixpkgs) lib;
 
-      mkSystems = sys: builtins.map (arch: "${arch}-${sys}") [ "x86_64" "aarch64" ];
-      systems = mkSystems "linux" ++ mkSystems "darwin";
+    mkSystems = sys: builtins.map (arch: "${arch}-${sys}") ["x86_64" "aarch64"];
+    systems = mkSystems "linux" ++ mkSystems "darwin";
 
-      forAllSystems = lib.genAttrs systems;
+    forAllSystems = lib.genAttrs systems;
 
-      nixpkgsFor = forAllSystems (system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default rust-overlay.overlays.default ];
-        });
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [self.overlays.default rust-overlay.overlays.default];
+      });
 
-      forEachSystem = fn: forAllSystems (system:
+    forEachSystem = fn:
+      forAllSystems (system:
         fn {
           inherit system;
           pkgs = nixpkgsFor.${system};
         });
 
-      toolchainFor = forEachSystem (p: p.pkgs.rust-bin.stable.latest.default);
-    in
-    {
-      checks = forEachSystem ({ pkgs, system }:
-        let
-          formatter = self.formatter.${system};
-        in
-        {
-          fmt = pkgs.runCommand "check-fmt" { }
-            ''
-              ${pkgs.lib.getExe formatter} --check ${self}
-              touch $out
-            '';
-        });
+    toolchainFor = forEachSystem (p: p.pkgs.rust-bin.stable.latest.default);
+  in {
+    checks = forEachSystem ({
+      pkgs,
+      system,
+    }: let
+      formatter = self.formatter.${system};
+    in {
+      fmt =
+        pkgs.runCommand "check-fmt" {}
+        ''
+          ${pkgs.lib.getExe formatter} --check ${self}
+          touch $out
+        '';
+    });
 
-      devShells = forEachSystem ({ pkgs, system }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rust-analyzer
-              toolchainFor.${system}
-            ];
+    devShells = forEachSystem ({
+      pkgs,
+      system,
+    }: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          rust-analyzer
+          toolchainFor.${system}
+        ];
 
-            RUST_BACKTRACE = 1;
-            RUST_SRC_PATH = "${toolchainFor.${system}}/lib/rustlib/src/rust/library";
-          };
-        });
-
-      formatter = forEachSystem (p: p.pkgs.nixpkgs-fmt);
-
-      packages = forEachSystem ({ pkgs, ... }: {
-        inherit (pkgs) nyoom;
-        default = pkgs.nyoom;
-      });
-
-      overlays.default = _: prev: {
-        nyoom = prev.callPackage
-          ({ libiconv, darwin, lib, lto ? true, optimizeSize ? true, pkg-config, rustPlatform, stdenv, version, self }:
-            rustPlatform.buildRustPackage
-              {
-                pname = "nyoom";
-                inherit version;
-
-                src = lib.sourceByRegex self [ "^src" "^presets" ".*\.rs$" ".*\.toml$" "^Cargo\.toml$" "^Cargo\.lock$" ];
-                cargoLock.lockFile = ./Cargo.lock;
-
-                RUSTFLAGS = ""
-                  + lib.optionalString lto " -C lto=thin -C embed-bitcode=yes"
-                  + lib.optionalString optimizeSize " -C codegen-units=1 -C strip=symbols -C opt-level=z";
-
-                buildInputs = [ ]
-                  ++ lib.optionals stdenv.isDarwin [
-                  darwin.apple_sdk_11_0.frameworks.CoreFoundation
-                  darwin.apple_sdk_11_0.frameworks.Security
-                  darwin.IOKit
-                  libiconv
-                ];
-
-                nativeBuildInputs = [ pkg-config ];
-              })
-          { inherit self version; };
+        RUST_BACKTRACE = 1;
+        RUST_SRC_PATH = "${toolchainFor.${system}}/lib/rustlib/src/rust/library";
       };
+    });
+
+    formatter = forEachSystem (p: p.pkgs.nixpkgs-fmt);
+
+    packages = forEachSystem ({pkgs, ...}: {
+      inherit (pkgs) nyoom;
+      default = pkgs.nyoom;
+    });
+
+    overlays.default = _: prev: {
+      nyoom =
+        prev.callPackage
+        ({
+          libiconv,
+          darwin,
+          lib,
+          lto ? true,
+          optimizeSize ? true,
+          pkg-config,
+          rustPlatform,
+          stdenv,
+          version,
+          system,
+          self,
+        }:
+          rustPlatform.buildRustPackage
+          {
+            pname = "nyoom";
+            inherit version;
+
+            src = crane.lib.${system}.cleanCargoSource ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            RUSTFLAGS =
+              ""
+              + lib.optionalString lto " -C lto=thin -C embed-bitcode=yes"
+              + lib.optionalString optimizeSize " -C codegen-units=1 -C strip=symbols -C opt-level=z";
+
+            buildInputs =
+              []
+              ++ lib.optionals stdenv.isDarwin [
+                darwin.apple_sdk_11_0.frameworks.CoreFoundation
+                darwin.apple_sdk_11_0.frameworks.Security
+                darwin.IOKit
+                libiconv
+              ];
+
+            nativeBuildInputs = [pkg-config];
+          })
+        {inherit self version;};
     };
+  };
 }
