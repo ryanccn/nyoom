af407eef4c1184306744f3c5d6ca06cc316cd4e9
Ryan Cao <hello@ryanccn.dev>
2025-10-24 19:58:59 +0100

    refactor: ferrix

--- flake.nix
+++ flake.nix
@@ -1,157 +1,16 @@
-# SPDX-FileCopyrightText: 2024 Ryan Cao <hello@ryanccn.dev>
+# SPDX-FileCopyrightText: 2025 Ryan Cao <hello@ryanccn.dev>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 {
-  description = "Small CLI Firefox userchrome manager";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    ferrix.url = "github:ryanccn/ferrix";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      inherit (nixpkgs) lib;
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      forAllSystems = lib.genAttrs systems;
-      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      checks = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-
-          mkFlakeCheck =
-            {
-              name,
-              command,
-              ...
-            }@args:
-            pkgs.stdenv.mkDerivation (
-              {
-                name = "check-${name}";
-                inherit (self.packages.${system}.nyoom) src;
-
-                buildPhase = ''
-                  ${command}
-                  touch "$out"
-                '';
-
-                doCheck = false;
-                dontInstall = true;
-                dontFixup = true;
-              }
-              // (removeAttrs args [
-                "name"
-                "command"
-              ])
-            );
-        in
-        {
-          nixfmt = mkFlakeCheck {
-            name = "nixfmt";
-            command = "find . -name '*.nix' -exec nixfmt --check {} +";
-
-            src = self;
-            nativeBuildInputs = with pkgs; [ nixfmt-rfc-style ];
-          };
-
-          rustfmt = mkFlakeCheck {
-            name = "rustfmt";
-            command = "cargo fmt --check";
-
-            nativeBuildInputs = with pkgs; [
-              cargo
-              rustfmt
-            ];
-          };
-
-          clippy = mkFlakeCheck {
-            name = "clippy";
-            command = ''
-              cargo clippy --all-features --all-targets --tests \
-                --offline --message-format=json \
-                | clippy-sarif | tee $out | sarif-fmt
-            '';
-
-            nativeBuildInputs = with pkgs; [
-              rustPlatform.cargoSetupHook
-              cargo
-              rustc
-              clippy
-              clippy-sarif
-              sarif-fmt
-            ];
-
-            inherit (self.packages.${system}.nyoom) cargoDeps;
-          };
-
-          reuse = mkFlakeCheck {
-            name = "reuse";
-            command = "reuse lint";
-
-            src = self;
-            nativeBuildInputs = with pkgs; [ reuse ];
-          };
-        }
-      );
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustfmt
-              clippy
-              rust-analyzer
-
-              reuse
-            ];
-
-            inputsFrom = [ self.packages.${system}.nyoom ];
-
-            env = {
-              RUST_BACKTRACE = 1;
-              RUST_SRC_PATH = toString pkgs.rustPlatform.rustLibSrc;
-            };
-          };
-        }
-      );
-
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-          packages = self.overlays.default null pkgs;
-        in
-        {
-          inherit (packages) nyoom;
-          default = packages.nyoom;
-        }
-      );
-
-      legacyPackages = forAllSystems (
-        system: nixpkgsFor.${system}.callPackage ./nix/static.nix { inherit self; }
-      );
-
-      overlays.default = _: prev: {
-        nyoom = prev.callPackage ./nix/package.nix { inherit self; };
-      };
-
-      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
+    { ferrix, ... }@inputs:
+    ferrix.lib.mkFlake inputs {
+      root = ./.;
     };
 }
